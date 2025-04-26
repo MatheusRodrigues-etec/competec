c40d5e22cc791b8133f1f0fadcc015f9b46f472e
MatheusRodrigues-etec <matheuscarneiro1779@gmail.com>
2025-04-26 11:26:16 -0300

    Update Main.java

--- aula-0/Main.java
+++ aula-0/Main.java
@@ -1,7 +1,27 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+	    
+	    Scanner entrada = new Scanner(System.in);
+	    
+	    System.out.print("Digite o valor da laranja:" );
+	    int laranja = entrada.nextInt();
+	    
+	    System.out.print("Digite o valor da uva:" );
+	    int uva = entrada.nextInt();
+	    
+	    System.out.print("Resultado:" + (laranja + uva));
+	    
+	    
+	    
+	    
 	}
 }
