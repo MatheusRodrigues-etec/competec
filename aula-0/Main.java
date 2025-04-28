e61e9ff3bb7070e5ac6973c7e21a3c073b441818
MatheusRodrigues-etec <matheuscarneiro1779@gmail.com>
2025-04-27 22:12:49 -0300

    Update Main.java

--- aula-0/Main.java
+++ aula-0/Main.java
@@ -1,47 +1,28 @@
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
 import java.util.Scanner;
-public class Main
-{
-	public static void main(String[] args) {
-	    
-	    Scanner entrada = new Scanner(System.in);
-	    
-	    System.out.print("Digite o valor da laranja:" );
-	    int laranja = entrada.nextInt();
-	    
-	    System.out.print("Digite o valor da uva:" );
-	    int uva = entrada.nextInt();
-	    
-	    System.out.println("Resultado:" + (laranja + uva));
-	    
-	    System.out.print("Digite a inicial do seu nome: ");
-        char nome = entrada.next().charAt(0);
-	   
-	    System.out.print("Digite o valor que voce tem no banco: ");
-	    double banco = entrada.nextDouble();
-	    
-	    System.out.print("Digite o valor da sua divida: ");
-	    double divida = entrada.nextDouble();
-	    
-	    System.out.println("Valor que sobrou apos a divida: " + (banco - divida));
-	    
-	    System.out.print("Digite o valor do produto:");
-	    float produto = entrada.nextFloat();
-	    
-	    System.out.print("Digite a quantidade de parcelas:");
-	    float parcelas = entrada.nextFloat();
-	    
-	    System.out.println("Valor de cada parcela:" + (produto / parcelas));
-	    
-	    System.out.print("Digite sua data de aniversario:");
-	    long aniversario = entrada.nextLong();
-	    
-	    
-	}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Informe o valor da laranja e da uva:");
+        int laranja = entrada.nextInt();
+        int uva = entrada.nextInt();
+        System.out.println("Soma dos valores: " + (laranja + uva));
+
+        System.out.print("Inicial do seu nome: ");
+        char inicial = entrada.next().charAt(0);
+
+        System.out.println("Informe seu saldo e o valor da dívida:");
+        double saldo = entrada.nextDouble();
+        double divida = entrada.nextDouble();
+        System.out.println("Saldo após dívida: " + (saldo - divida));
+
+        System.out.println("Informe o valor do produto e o número de parcelas:");
+        float produto = entrada.nextFloat();
+        int parcelas = entrada.nextInt();
+        System.out.println("Valor de cada parcela: " + (produto / parcelas));
+
+        System.out.print("Data de aniversário (ex: 20050510): ");
+        long aniversario = entrada.nextLong();
+    }
 }
