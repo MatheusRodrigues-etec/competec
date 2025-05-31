f4dbca63041d56203e77ab5c2862ab38f112b77d
MatheusRodrigues-etec <matheuscarneiro1779@gmail.com>
2025-05-31 00:03:39 -0300

    Update Main.java

--- aula-3/Main.java
+++ aula-3/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         
        Scanner entrada = new Scanner (System.in);
-       /*
+       
        int y = 0;
        String nome = entrada.nextLine();
        
@@ -25,7 +25,7 @@ public class Main {
        }
        
        System.out.println("Valor total:" +soma);
-       */
+       
       
       // tabuada * 1
       // tabuada * 2
@@ -37,18 +37,18 @@ public class Main {
       for (int i = 1; i <= 10; i++) {
           System.out.println( i*tabuada);
       }
-      */
+      
           import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
      
      Scanner entrada = new Scanner(System.in);
-      /*
+      
       for (int pares = 2; pares <= 20; pares += 2) {    
           System.out.println("Os pares são: " + pares);
       }
-      */
+      
       
       int n = 10;
       
@@ -68,4 +68,43 @@ public class Main {
       
     }
     
+}
+*/
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+     
+     Scanner entrada = new Scanner(System.in);
+      /*
+      for (int pares = 2; pares <= 20; pares += 2) {    
+          System.out.println("Os pares são: " + pares);
+      }
+      
+      
+      int n = 10;
+      
+      while (n > 0) {   
+          System.out.println("Descendo...: " + n);
+          n--;
+      }
+          System.out.println("Numeros Esgotados.");
+      */
+          System.out.print("Digite um Numero: ");
+      int acesso = entrada.nextInt();
+      int contador = 0;    
+      
+      
+       while (acesso != 0) {    
+           contador++;
+           System.out.print("Digite outro Numero: ");
+           acesso = entrada.nextInt();
+           
+       }
+      
+      System.out.println("Você digitou " + contador + " Numeros diferentes de zero.");
+      entrada.close();
+      
+    }
 }
