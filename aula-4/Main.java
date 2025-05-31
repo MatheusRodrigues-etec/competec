19f94228ac0adbbcf76470c154af20169955d5f0
MatheusRodrigues-etec <matheuscarneiro1779@gmail.com>
2025-05-31 11:13:17 -0300

    Update Main.java

--- aula-4/Main.java
+++ aula-4/Main.java
@@ -1,7 +1,20 @@
-public class Main
-{
-	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
-	}
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {    
+        
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("insira o tamanho do array: ");
+        int num = entrada.nextInt();
+        int arranjo[] = new int [num];
+        
+        for(int i =0; i <arranjo.length; i++){  
+            arranjo[i] = entrada.nextInt( );
+        }
+        
+        for(int i =0; i <arranjo.length; i++){  
+            System.out.print(arranjo[i] + " ");
+        }
+        
+    }
 }
