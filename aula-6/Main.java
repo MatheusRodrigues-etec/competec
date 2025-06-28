3bc6f611a0d64173a0413a3dc35502bac9b02de6
MatheusRodrigues-etec <matheuscarneiro1779@gmail.com>
2025-06-28 11:20:23 -0300

    Update Main.java

--- aula-6/Main.java
+++ aula-6/Main.java
@@ -1,7 +1,43 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+	
+	Scanner entrada = new Scanner (System.in);
+	/*
+	System.out.print("Digite seu Primeiro Nome: ");
+	String nome = entrada.nextLine();
+	
+	int tamanho = nome.length();
+	
+	System.out.print("Olá, Seu Primeiro nome é: " + nome + " e tem " + tamanho + " Caracteres");
+	*/
+	
+	/*System.out.print("Qual a palavra SECRETA? ");
+	String secreta = "BATATA";
+	String palavra = entrada.nextLine();
+	
+	boolean resultado =(palavra.equalsIgnoreCase(secreta)); 
+    if (resultado == true){ 
+        System.out.println("Parabéns, Você Acertou!");
+    }else { 
+        System.out.println("Você errou, Tente novamente");
+    }*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 }
